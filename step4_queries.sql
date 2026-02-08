53baf2a1a7ac95c8853df05ee0dfb6d7065c8996
Shad-code725 <nischadr@gmail.com>
2026-02-08 19:50:44 +0200

    Add LEFT JOIN for Flights and Bookings
    Added LEFT JOIN query to retrieve flight and booking details.

--- step4_queries.sql
+++ step4_queries.sql
@@ -3,3 +3,9 @@ SELECT c.customer_id, c.name, b.booking_id, b.amount_paid
 FROM Customers c
 INNER JOIN Bookings b
 ON c.customer_id = b.customer_id;
+
+-- LEFT JOIN: Flights & Bookings
+SELECT f.flight_id, f.route, b.booking_id, b.amount_paid
+FROM Flights f
+LEFT JOIN Bookings b
+ON f.flight_id = b.flight_id;
