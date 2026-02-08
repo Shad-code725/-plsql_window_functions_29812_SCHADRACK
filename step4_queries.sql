9582d1263f1af09462401d3558d6a0203412ed1f
Shad-code725 <nischadr@gmail.com>
2026-02-08 20:40:30 +0200

    Update step4_queries.sql

--- step4_queries.sql
+++ step4_queries.sql
@@ -21,3 +21,15 @@ SELECT f.flight_id, f.route, b.booking_id, b.amount_paid
 FROM Flights f
 FULL OUTER JOIN Bookings b
 ON f.flight_id = b.flight_id;
+
+-- SELF JOIN: Customers (find pairs with same city)
+
+SELECT c1.customer_id AS customer_a_id,
+c1.name AS customer_a_name,
+c2.customer_id AS customer_b_id,
+c2.name AS customer_b_name,
+c1.region
+FROM Customers c1
+INNER JOIN Customers c2
+  ON c1.region = c2.region
+  AND c1.customer_id <> c2.customer_id;
